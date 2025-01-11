6abc544a78951a6ecf9e9a8166f099db43418144
ALI <aliatyat25@gmail.com>
2025-01-11 06:33:48 -0800

    working on

--- main.c
+++ main.c
@@ -27,18 +27,29 @@ int main(int argc, char **argv)
     t_stack *b = init_stack();
 
     if (argc < 2 || !validate_input(argc, argv))
-        return (write(2, "Error\n", 6), free_stack(a), free_stack(b), 1);
+    {
+        write(2, "Error\n", 6);
+        free_stack(a);
+        free_stack(b);
+        return (1);
+    }
 
-    for (int i = argc - 1; i > 0; i--)
+    int i = argc - 1;
+    while (i > 0)
     {
         t_node *new_node = malloc(sizeof(t_node));
         new_node->value = ft_atoi(argv[i]);
         new_node->next = a->top;
         a->top = new_node;
+        i--;
     }
 
     if (is_sorted(a))
-        return (free_stack(a), free_stack(b), 0);
+    {
+        free_stack(a);
+        free_stack(b);
+        return (0);
+    }
 
     if (stack_size(a) <= 3)
         sort_three(a);
